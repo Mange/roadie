dfc3903fb093484d1770ca8d838551d9377d1a85
Magnus Bergmark <magnus.bergmark@gmail.com>
2011-06-24 08:21:04 +0200

    Add #guess to CSSFileNotFound error
    I'm going to use it when guessing a local file
    path from an URL. This should help any application
    developers getting this error.

--- lib/roadie/css_file_not_found.rb
+++ lib/roadie/css_file_not_found.rb
@@ -2,11 +2,21 @@ module Roadie
   # Raised when a stylesheet specified for inlining is not present.
   # You can access the target filename via #filename.
   class CSSFileNotFound < StandardError
-    attr_reader :filename
+    attr_reader :filename, :guess
 
-    def initialize(filename)
+    def initialize(filename, guess = nil)
       @filename = filename
-      super("Could not find #{filename}")
+      @guess = guess
+      super(build_message)
     end
+
+    private
+      def build_message
+        if guess
+          "Could not find #{filename} (guessed from #{guess.inspect})"
+        else
+          "Could not find #{filename}"
+        end
+      end
   end
 end
--- spec/lib/roadie/css_file_not_found_spec.rb
+++ spec/lib/roadie/css_file_not_found_spec.rb
@@ -6,8 +6,24 @@ module Roadie
       CSSFileNotFound.new('file.css').filename.should == 'file.css'
     end
 
-    it "has a message" do
-      CSSFileNotFound.new('style.css').message.should == 'Could not find style.css'
+    it "can be initialized with the guess the filename was based on" do
+      CSSFileNotFound.new('file.css', :file).guess.should == :file
+    end
+
+    it "has a nil guess when no guess was specified" do
+      CSSFileNotFound.new('').guess.should be_nil
+    end
+
+    context "without a guess" do
+      it "has a message with the wanted filename" do
+        CSSFileNotFound.new('style.css').message.should == 'Could not find style.css'
+      end
+    end
+
+    context "with a guess" do
+      it "has a message with the wanted filename and the guess" do
+        CSSFileNotFound.new('style.css', :style).message.should == 'Could not find style.css (guessed from :style)'
+      end
     end
   end
 end
