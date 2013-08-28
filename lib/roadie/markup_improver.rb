193560b993bdb3128cc14df009cbd15d964b8e46
Magnus Bergmark <magnus.bergmark@gmail.com>
2013-08-28 09:26:48 +0200

    MarkupImprover: Reword and expand examples, document, refactor

--- lib/roadie/markup_improver.rb
+++ lib/roadie/markup_improver.rb
@@ -1,42 +1,61 @@
 module Roadie
+  # Class that improves the markup of a HTML DOM tree
+  #
+  # This class will improve the following aspects of the DOM:
+  #   * A HTML5 doctype will be added if missing, other doctypes will be left as-is
+  #   * Basic HTML elements will be added if missing
+  #     * <html>
+  #     * <head>
+  #     * <body>
+  #     * <meta> declaring charset and content-type (text/html)
+  #
+  # The original HTML must also be passed in in order to handle the doctypes
+  # since a Nokogiri::HTML::Document will always have a doctype, no matter if
+  # the original source had it or not. Reading the raw HTML is the only way to
+  # determine if we want to add a HTML5 doctype or not.
   class MarkupImprover
     def initialize(dom, original_html)
       @dom = dom
       @html = original_html
     end
 
+    # @return [nil] passed DOM will be mutated
     def improve
-      ensure_doctype
-      head = ensure_head_element
-      ensure_charset_element head
+      ensure_doctype_present
+      head = ensure_head_element_present
+      ensure_declared_charset head
     end
 
     private
     attr_reader :dom
 
-    def ensure_doctype
+    def ensure_doctype_present
       return if @html.include?('<!DOCTYPE ')
       # Nokogiri adds a "default" doctype to the DOM, which we will remove
       dom.internal_subset.remove unless dom.internal_subset.nil?
       dom.create_internal_subset 'html', nil, nil
     end
 
-    def ensure_head_element
+    def ensure_head_element_present
       if (head = dom.at_xpath('html/head'))
         head
       else
-        head = Nokogiri::XML::Node.new('head', dom)
-        html = dom.at_xpath('html')
-        if html.children.size > 0
-          html.children.before(head)
-        else
-          html << head
-        end
-        head
+        create_head_element dom.at_xpath('html')
       end
     end
 
-    def ensure_charset_element(parent)
+    def create_head_element(parent)
+      head = Nokogiri::XML::Node.new 'head', dom
+      unless parent.children.empty?
+        # Crashes when no children are present
+        parent.children.before head
+      else
+        parent << head
+      end
+      head
+    end
+
+    def ensure_declared_charset(parent)
       if content_type_meta_element_missing?
         parent.add_child make_content_type_element
       end
--- spec/markup_improver_spec.rb
+++ spec/markup_improver_spec.rb
@@ -9,33 +9,6 @@ module Roadie
       dom
     end
 
-    it "inserts <head> if not present" do
-      improve('<html><body></body></html>').should have_selector('html > head + body')
-    end
-
-    it "inserts basic html structure if not present" do
-      improve('<h1>Hey!</h1>').should have_selector('html > head + body > h1')
-    end
-
-    it "inserts meta tag describing content-type" do
-      dom = improve('<html><head></head><body></body></html>')
-
-      dom.should have_selector('head meta')
-      meta = dom.at_css('head meta')
-      meta['http-equiv'].should == 'Content-Type'
-      meta['content'].should == 'text/html; charset=UTF-8'
-    end
-
-    it "does not insert duplicate meta tags describing content-type" do
-      improve(<<-HTML).xpath('//meta').should have(1).item
-      <html>
-        <head>
-          <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
-        </head>
-      </html>
-      HTML
-    end
-
     describe "automatic doctype" do
       it "inserts a HTML5 doctype if no doctype is present" do
         improve("<html></html>").internal_subset.to_xml.should == "<!DOCTYPE html>"
@@ -50,6 +23,47 @@ module Roadie
         dtd = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">"
         html = "#{dtd}<html></html>"
         improve(html).internal_subset.to_xml.should == dtd
+      end
+    end
+
+    describe "basic HTML structure" do
+      it "inserts a <html> element as the root" do
+        improve("<h1>Hey!</h1>").should have_selector("html h1")
+        improve("<html></html>").css('html').size.should == 1
+      end
+
+      it "inserts <head> if not present" do
+        improve('<html><body></body></html>').should have_selector('html > head + body')
+        improve('<html></html>').should have_selector('html > head')
+        improve('Foo').should have_selector('html > head')
+        improve('<html><head></head></html>').css('head').size.should == 1
+      end
+
+      it "inserts <body> if not present" do
+        improve('<h1>Hey!</h1>').should have_selector('html > body > h1')
+        improve('<html><h1>Hey!</h1></html>').should have_selector('html > body > h1')
+        improve('<html><body><h1>Hey!</h1></body></html>').css('body').size.should == 1
+      end
+    end
+
+    describe "charset declaration" do
+      it "is inserted if missing" do
+        dom = improve('<html><head></head><body></body></html>')
+
+        dom.should have_selector('head meta')
+        meta = dom.at_css('head meta')
+        meta['http-equiv'].should == 'Content-Type'
+        meta['content'].should == 'text/html; charset=UTF-8'
+      end
+
+      it "is left alone when predefined" do
+        improve(<<-HTML).xpath('//meta').should have(1).item
+        <html>
+          <head>
+            <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+          </head>
+        </html>
+        HTML
       end
     end
   end
