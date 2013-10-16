7378302209229db7496faee193407f4706ba6fa8
Magnus Bergmark <magnus.bergmark@gmail.com>
2013-10-16 08:36:14 +0200

    Remove Roadie module methods

--- lib/roadie.rb
+++ lib/roadie.rb
@@ -1,41 +1,4 @@
 module Roadie
-  class << self
-    def inline_css(assets, targets, html, url_options, after_inlining_handler = nil)
-      document = Document.new(html)
-      # Note: Targets are not passed anymore; they are to be removed - all
-      # stylesheets should be given explicitly and/or be referenced from the
-      # HTML.
-      document.asset_providers = [assets]
-      document.url_options = url_options
-      document.after_inlining = after_inlining_handler
-      document.transform
-    end
-
-    # Shortcut to Rails.application
-    def app
-      Rails.application
-    end
-
-    # Returns the value of +config.roadie.enabled+.
-    #
-    # Roadie will disable all processing if this config is set to +false+. If
-    # you just want to disable CSS inlining without disabling the rest of
-    # Roadie, pass +css: nil+ to the +defaults+ method inside your mailers.
-    def enabled?
-      config.roadie.enabled
-    end
-
-    # Returns the value of +config.roadie.after_inlining+
-    #
-    def after_inlining_handler
-      config.roadie.after_inlining
-    end
-
-    private
-      def config
-        Roadie.app.config
-      end
-  end
 end
 
 require 'roadie/version'
