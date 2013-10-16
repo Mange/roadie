15eea59a548e2dc8f96224c9e5eda6395af043f0
Magnus Bergmark <magnus.bergmark@gmail.com>
2013-10-16 09:01:03 +0200

    Refactor and improve readability of Inliner code

--- lib/roadie/inliner.rb
+++ lib/roadie/inliner.rb
@@ -16,29 +16,37 @@ module Roadie
     # @param [Nokogiri::HTML::Document] dom
     # @return [nil]
     def inline(dom)
-      style_map = StyleMap.new
-
-      stylesheet.each_inlinable_block do |selector, properties|
-        style_map.add_elements elements_in_selector(selector, dom), properties
-      end
-
-      style_map.each_element do |element, rules|
-        apply_element_style element, rules
-      end
-
+      apply style_map(dom)
       nil
     end
 
     private
     attr_reader :stylesheet
 
-    def apply_element_style(element, rules)
-      element["style"] = [rules.to_s, element["style"]].compact.join(";")
+    def apply(style_map)
+      style_map.each_element do |element, properties|
+        apply_element_style element, properties
+      end
     end
 
-    def elements_in_selector(selector, dom)
+    def style_map(dom)
+      style_map = StyleMap.new
+
+      stylesheet.each_inlinable_block do |selector, properties|
+        elements = elements_matching_selector(selector, dom)
+        style_map.add elements, properties
+      end
+
+      style_map
+    end
+
+    def apply_element_style(element, properties)
+      element["style"] = [properties.to_s, element["style"]].compact.join(";")
+    end
+
+    def elements_matching_selector(selector, dom)
       dom.css(selector.to_s)
-    # There's no way to get a list of supported pseudo rules, so we're left
+    # There's no way to get a list of supported pseudo selectors, so we're left
     # with having to rescue errors.
     # Pseudo selectors that are known to be bad are skipped automatically but
     # this will catch the rest.
@@ -53,20 +61,19 @@ module Roadie
 
     class StyleMap
       def initialize
-        @map = {}
+        @map = Hash.new { |hash, key|
+          hash[key] = StyleProperties.new([])
+        }
       end
 
-      def add_elements(elements, new_properties)
-        elements.each { |element| add(element, new_properties) }
-      end
-
-      def add(element, new_properties)
-        properties = (@map[element] ||= StyleProperties.new([]))
-        properties.merge!(new_properties)
+      def add(elements, new_properties)
+        Array(elements).each do |element|
+          @map[element].merge!(new_properties)
+        end
       end
 
       def each_element
-        @map.each_pair { |element, rules| yield element, rules }
+        @map.each_pair { |element, properties| yield element, properties }
       end
     end
   end
