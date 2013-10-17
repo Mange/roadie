c7539f44498366f9d34b88279d54df6a89e19196
Magnus Bergmark <magnus.bergmark@gmail.com>
2013-10-17 16:03:34 +0200

    Oops. NullProvider were still providing strings instead of Stylesheets

--- lib/roadie/null_provider.rb
+++ lib/roadie/null_provider.rb
@@ -1,6 +1,9 @@
 module Roadie
   class NullProvider
-    def find_stylesheet(name) "" end
-    def find_stylesheet!(name) "" end
+    def find_stylesheet(name) empty_stylesheet end
+    def find_stylesheet!(name) empty_stylesheet end
+
+    private
+    def empty_stylesheet() Stylesheet.new "(null)", "" end
   end
 end
--- spec/lib/roadie/null_provider_spec.rb
+++ spec/lib/roadie/null_provider_spec.rb
@@ -6,9 +6,16 @@ module Roadie
   describe NullProvider do
     it_behaves_like "asset provider role"
 
-    it "finds the empty string for every name" do
-      NullProvider.new.find_stylesheet("omg wtf bbq").should == ""
-      NullProvider.new.find_stylesheet!("omg wtf bbq").should == ""
+    def expect_empty_stylesheet(stylesheet)
+      stylesheet.should_not be_nil
+      stylesheet.name.should == "(null)"
+      stylesheet.should have(0).blocks
+      stylesheet.to_s.should be_empty
+    end
+
+    it "finds an empty stylesheet for every name" do
+      expect_empty_stylesheet NullProvider.new.find_stylesheet("omg wtf bbq")
+      expect_empty_stylesheet NullProvider.new.find_stylesheet!("omg wtf bbq")
     end
   end
 end
