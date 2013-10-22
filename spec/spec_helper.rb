0a15c7a5349914921c07fb1f221ff652422df897
Magnus Bergmark <magnus.bergmark@gmail.com>
2013-10-22 08:48:49 +0200

    Remove code that shouldn't be in the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,5 @@
 $: << File.dirname(__FILE__) + '/../lib'
 
-require 'ostruct'
-require 'rubygems'
-require 'bundler'
-
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-
-require 'rspec'
 require 'roadie'
 
 RSpec.configure do |config|
