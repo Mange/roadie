599badda8a1978f6f5f69fa641a4134f4c4e957f
Magnus Bergmark <magnus.bergmark@gmail.com>
2015-06-16 00:02:54 +0200

    Allow PathRewriterProvider to skip some assets completely

--- lib/roadie/path_rewriter_provider.rb
+++ lib/roadie/path_rewriter_provider.rb
@@ -1,16 +1,24 @@
 module Roadie
   class PathRewriterProvider
-    def initialize(provider, &block)
+    attr_reader :provider, :filter
+
+    def initialize(provider, &filter)
       @provider = provider
-      @block = block
+      @filter = filter
     end
 
     def find_stylesheet(path)
-      @provider.find_stylesheet(@block.call(path))
+      new_path = filter.call(path)
+      provider.find_stylesheet(new_path) if new_path
     end
 
     def find_stylesheet!(path)
-      @provider.find_stylesheet!(@block.call(path))
+      new_path = filter.call(path)
+      if new_path
+        provider.find_stylesheet!(new_path)
+      else
+        raise CssNotFound, "Filter returned #{new_path.inspect}"
+      end
     end
   end
 end
--- spec/lib/roadie/path_rewriter_provider_spec.rb
+++ spec/lib/roadie/path_rewriter_provider_spec.rb
@@ -13,5 +13,27 @@ module Roadie
     end
 
     it_behaves_like "roadie asset provider", valid_name: "well.css", invalid_name: "bad"
+
+    it "does not call the upstream provider if block returns nil" do
+      provider = PathRewriterProvider.new(upstream) { nil }
+      expect(upstream).to_not receive(:find_stylesheet)
+      expect(upstream).to_not receive(:find_stylesheet!)
+
+      expect(provider.find_stylesheet("foo")).to be_nil
+      expect {
+        provider.find_stylesheet!("foo")
+      }.to raise_error(CssNotFound, /nil/)
+    end
+
+    it "does not call the upstream provider if block returns false" do
+      provider = PathRewriterProvider.new(upstream) { false }
+      expect(upstream).to_not receive(:find_stylesheet)
+      expect(upstream).to_not receive(:find_stylesheet!)
+
+      expect(provider.find_stylesheet("foo")).to be_nil
+      expect {
+        provider.find_stylesheet!("foo")
+      }.to raise_error(CssNotFound, /false/)
+    end
   end
 end
