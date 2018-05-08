89141c7fa1bb8545aaf4e835345c6a591d0b93bd
Jeremy Nagel <jnagel@cozero.com.au>
2018-05-08 17:13:48 +1000

    Action feedback from Mange

--- lib/roadie/style_block.rb
+++ lib/roadie/style_block.rb
@@ -9,6 +9,9 @@ module Roadie
 
     # @param [Selector] selector
     # @param [Array<StyleProperty>] properties
+    # @param [Array<?>] media  Array of media types, e.g.
+    #                          @media screen, print and (max-width 800px) will become
+    #                          [:screen, :"print and (max-width 800px)"]
     def initialize(selector, properties, media)
       @selector = selector
       @properties = properties
@@ -22,20 +25,44 @@ module Roadie
     #   @see Selector#to_s
     def_delegator :selector, :to_s, :selector_string
 
-    # String representation of the style block. This is valid CSS and can be
-    # used in the DOM.
+    # Checks whether the media query can be inlined (e.g. media)
     def inlinable?
       inlinable_media? && selector.inlinable?
     end
 
+    # String representation of the style block. This is valid CSS and can be
+    # used in the DOM.
     def to_s
-      "#{selector}{#{properties.map(&:to_s).join(';')}}"
+      css_rules = "#{selector}{#{properties.map(&:to_s).join(';')}}"
+      if inlinable_media?
+        css_rules
+      else
+        "#{media_query} { #{css_rules} }"
+      end
     end
 
     private
 
+    # Returns media queries that can be placed in a style tag
+    # e.g. @media print, screen and (max-width: 600px)
+    # @return {String}
+    def media_query
+      unless inlinable_media?
+        # NB - @media is an array of media types. e.g.
+        # @media screen, print and (max-width 800px) will become
+        # [:screen, :"print and (max-width 800px)"]
+        # Therefore when we resurrect the array into a media query, use
+        # .join(', ')
+        "@media #{@media.map(&:to_s).join(', ')}"
+      end
+    end
+
+    # A media query cannot be inlined if it contains any advanced rules
+    # e.g. @media only screen {...} is ok to inline but
+    # @media only screen and (max-width: 600px) {...} cannot be inlined
+    # @return {Boolean}
     def inlinable_media?
-      @media.all? {|media_query| media_query.to_s.count('()') < 2 }
+      @media.none? { |media_query| media_query.to_s.include? '(' }
     end
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -526,4 +526,41 @@ describe "Roadie functionality" do
     result = parse_html document.transform
     expect(result).to have_styling('color' => 'green').at_selector('.colorful')
   end
+
+  it "should put non-inlineable media queries in the head" do
+    document = Roadie::Document.new <<-HTML
+      <html>
+        <head>
+          <link rel="stylesheet" href="/style.css">
+        </head>
+        <body>
+          <div class="colorful"></div>
+        </body>
+      </html>
+    HTML
+
+    document.asset_providers = TestProvider.new(
+      "/style.css" => <<-CSS
+        .colorful { color: green; }
+        @media screen and (max-width 600px) {
+          .colorful { color: red; width: 600px; }
+          .colorful-2 { color: red; width: 600px; }
+        }
+        @media screen and (max-width 800px) {
+          .colorful { color: blue; }
+        }
+        @media screen, print and (max-width 800px) {
+          .colorful { color: blue; }
+        }
+      CSS
+    )
+
+    result = parse_html document.transform
+
+    styles = result.at_css('html > head > style').text
+    expect(styles).to eq('@media screen and (max-width 600px) { .colorful{color:red;width:600px} }
+@media screen and (max-width 600px) { .colorful-2{color:red;width:600px} }
+@media screen and (max-width 800px) { .colorful{color:blue} }
+@media screen, print and (max-width 800px) { .colorful{color:blue} }')
+  end
 end
