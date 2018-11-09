0e2f685712ce18245a592bcd0f7a89f513de07ae
Mikhail Doronin <mikhail.doronin@capitainetrain.com>
2018-11-09 18:44:21 +0100

    Make document.rb compatible with frozen literals

--- lib/roadie/document.rb
+++ lib/roadie/document.rb
@@ -50,14 +50,14 @@ module Roadie
       @html = html
       @asset_providers = ProviderList.wrap(FilesystemProvider.new)
       @external_asset_providers = ProviderList.empty
-      @css = ""
+      @css = String.new("").force_encoding('UTF-8')
       @mode = :html
     end
 
     # Append additional CSS to the document's internal stylesheet.
     # @param [String] new_css
     def add_css(new_css)
-      @css << "\n\n" << new_css
+      @css << "\n\n" + new_css
     end
 
     # Transform the input HTML as a full document and returns the processed
